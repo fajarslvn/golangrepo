c46fdcb15d635ecdf2d52feb8e515b8d5685bfb1
fajarslvn <jarphish@gmail.com>
2021-10-28 08:00:42 +0700

    init

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package goroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -24,7 +25,7 @@ func TestCreateChannel(t *testing.T) {
 
 func GiveMeResponse(channel chan string) {
 	time.Sleep(2 * time.Second)
-	channel <- "Data sent!"
+	channel <- "- Data sent!"
 	fmt.Println("Finish sending data to channel")
 }
 
@@ -78,4 +79,47 @@ func TestBufferedChannel(t *testing.T) {
 
 	time.Sleep(2 * time.Second)
 	fmt.Println("End of Buffer")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "loop at " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Receiving data", data)
+	}
+	fmt.Println("DONE!")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data from channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data from channel 2", data)
+			counter++
+		default:
+			fmt.Println("Waiting for data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
 }
