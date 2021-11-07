9bc585833db0f01cc5d87a0ebfc03d0eb22ca301
fajarslvn <jarphish@gmail.com>
2021-11-07 17:04:07 +0700

    Solved

--- module01/dec_to_base.go
+++ module01/dec_to_base.go
@@ -10,5 +10,25 @@ package module01
 //   DecToBase(14, 2) => "1110"
 //
 func DecToBase(dec, base int) string {
-	return ""
+	const charset = "0123456789ABCDEF"
+	var result string
+
+	for dec > 0 {
+		remainder := dec % base
+		result = string(charset[remainder]) + result
+		dec = dec / base
+	}
+	return result
 }
+
+/* Solution 2 with Reverse function from reverse.go
+const charset = "0123456789ABCDEF"
+	var sb strings.Builder
+
+	for dec > 0 {
+		remainder := dec % base
+		sb.WriteByte(charset[remainder])
+		dec = dec / base
+	}
+	return Reverse(sb.String())
+*/
