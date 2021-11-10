24d405e4b92d8c17b3c848d74189867987b70d0b
fajarslvn <jarphish@gmail.com>
2021-11-10 21:06:28 +0700

    init

--- sql_test.go
+++ sql_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -86,5 +87,97 @@ func TestQuerySqlComplex(t *testing.T) {
 		}
 		fmt.Println("Married:", married)
 		fmt.Println("Created At:", createdAt)
+	}
+}
+
+func TestSqlInjectionSafe(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	username := "admin"
+	password := "admin"
+
+	SqlQuery := "SELECT username FROM user WHERE username = ? AND password = ? LIMIT 1"
+	fmt.Println(SqlQuery)
+
+	rows, err := db.QueryContext(ctx, SqlQuery, username, password)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Success insert new customer")
+	defer rows.Close()
+
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Login Successfuly!", username)
+	} else {
+		fmt.Println("Login Failed!")
+	}
+}
+
+func TestExecSqlParameter(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	username := "Johny"
+	password := 1111111
+
+	ctx := context.Background()
+	sqlQuery := "INSERT INTO user(username, password) VALUES(?, ?)"
+	_, err := db.ExecContext(ctx, sqlQuery, username, password)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Success insert new user")
+}
+
+func TestAutoIncrement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	email := "world@world.com"
+	comment := "You are Awesome!"
+
+	ctx := context.Background()
+	sqlQuery := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+	result, err := db.ExecContext(ctx, sqlQuery, email, comment)
+	if err != nil {
+		panic(err)
+	}
+	insertId, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Last Insert Id:", insertId)
+}
+
+func TestPrepareStatement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	sqlQuery := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+	statement, err := db.PrepareContext(ctx, sqlQuery)
+	if err != nil {
+		panic(err)
+	}
+	defer statement.Close()
+
+	for i := 0; i < 10; i++ {
+		email := "jay" + strconv.Itoa(i) + "@gmail.com"
+		comment := "This is comment number " + strconv.Itoa(i)
+		result, err := statement.ExecContext(ctx, email, comment)
+		if err != nil {
+			panic(err)
+		}
+		lastInsertId, _ := result.LastInsertId()
+		fmt.Println("Comment Id", lastInsertId)
 	}
 }
