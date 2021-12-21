3d5b503be7744826a94c1849acaa5c4ac3549597
fajarslvn <jarphish@gmail.com>
2021-12-21 20:39:51 +0700

    init

--- web/xss_test.go
+++ web/xss_test.go
@@ -2,6 +2,7 @@ package go_web
 
 import (
 	"fmt"
+	"html/template"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -11,7 +12,7 @@ import (
 func TemplateAutoEscape(w http.ResponseWriter, r *http.Request) {
 	myTemplates.ExecuteTemplate(w, "post.gohtml", map[string]interface{}{
 		"Title": "Go-Lang Auto Escape",
-		"Body":  "<p>Selamat belajar golang web</p>",
+		"Body":  "<p>Selamat belajar golang web<script>alert(9)</script></p>",
 	})
 }
 
@@ -27,4 +28,49 @@ func TestTemplateAutoEscape(t *testing.T) {
 		panic(err)
 	}
 	fmt.Println(string(body))
+}
+
+func TestTemplateAutoEscapeServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8181",
+		Handler: http.HandlerFunc(TemplateAutoEscape),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TemplateAutoEscapeDisabled(w http.ResponseWriter, r *http.Request) {
+	myTemplates.ExecuteTemplate(w, "post.gohtml", map[string]interface{}{
+		"Title": "Go-Lang Auto Escape",
+		"Body":  template.HTML("<p>Selamat belajar golang web</p>"),
+	})
+}
+
+func TestTemplateAutoEscapeDisabled(t *testing.T) {
+	req := httptest.NewRequest("GET", "http://localhost:8181", nil)
+	rec := httptest.NewRecorder()
+
+	TemplateAutoEscapeDisabled(rec, req)
+
+	res := rec.Result()
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(body))
+}
+
+func TestTemplateAutoEscapeDisabledServer(t *testing.T) {
+	server := http.Server{
+		Addr:    "localhost:8181",
+		Handler: http.HandlerFunc(TemplateAutoEscapeDisabled),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
