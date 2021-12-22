c039d3ecc1c759628871baeb06df5534f31744f6
fajarslvn <jarphish@gmail.com>
2021-12-22 21:00:52 +0700

    init

--- web/upload_file_test.go
+++ web/upload_file_test.go
@@ -1,7 +1,9 @@
 package go_web
 
 import (
+	"io"
 	"net/http"
+	"os"
 	"testing"
 )
 
@@ -10,6 +12,27 @@ func UploadForm(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func Upload(w http.ResponseWriter, r *http.Request) {
+	// r.ParseMultiPlatform(32 << 20)
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		panic(err)
+	}
+	fileDestination, err := os.Create("./resources/" + fileHeader.Filename)
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(fileDestination, file)
+	if err != nil {
+		panic(err)
+	}
+	name := r.PostFormValue("name")
+	myTemplates.ExecuteTemplate(w, "upload.form.gohtml", map[string]interface{}{
+		"Name": name,
+		"File": "/static/" + fileHeader.Filename,
+	})
 }
 
 func TestUploadForm(t *testing.T) {
