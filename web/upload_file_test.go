c778baea671acd972f10794c7056fa541108989a
fajarslvn <jarphish@gmail.com>
2021-12-25 09:39:21 +0700

    init

--- web/upload_file_test.go
+++ web/upload_file_test.go
@@ -15,21 +15,25 @@ func UploadForm(w http.ResponseWriter, r *http.Request) {
 }
 
 func Upload(w http.ResponseWriter, r *http.Request) {
+	// input/post a file from a html form
 	// r.ParseMultiPlatform(32 << 20)
 	file, fileHeader, err := r.FormFile("file")
 	if err != nil {
 		panic(err)
 	}
+	// Create the file and decide the destination folder (./resources/)
 	fileDestination, err := os.Create("./resources/" + fileHeader.Filename)
 	if err != nil {
 		panic(err)
 	}
+	// Save file to destination folder
 	_, err = io.Copy(fileDestination, file)
 	if err != nil {
 		panic(err)
 	}
+	// Build success template by getting file name
 	name := r.PostFormValue("name")
-	myTemplates.ExecuteTemplate(w, "upload.form.gohtml", map[string]interface{}{
+	myTemplates.ExecuteTemplate(w, "upload.success.gohtml", map[string]interface{}{
 		"Name": name,
 		"File": "/static/" + fileHeader.Filename,
 	})
@@ -37,7 +41,12 @@ func Upload(w http.ResponseWriter, r *http.Request) {
 
 func TestUploadForm(t *testing.T) {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/form", UploadForm)
+	mux.HandleFunc("/", UploadForm)
+	mux.HandleFunc("/upload", Upload)
+	mux.Handle("/static/",
+		http.StripPrefix("/static",
+			http.FileServer(
+				http.Dir("./resources"))))
 
 	server := http.Server{
 		Addr:    "localhost:8181",
